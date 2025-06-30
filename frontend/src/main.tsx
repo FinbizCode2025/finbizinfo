b419381ccba8d7f0a4f847cf0537dc4c492db35f
Pratik Raj <123pratikraj1089@gmail.com>
2025-06-30 16:40:21 +0530

    main.tsx change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '../backend/App';
+import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chatus from './components/chatbot/Main';
